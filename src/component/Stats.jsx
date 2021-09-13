e004b101fe6416b1743273437702c775b1011e1b
Priya Premprakash Babu <piyush02072002@gmail.com>
2021-09-13 12:51:05 +0530

    update

--- src/component/Stats.jsx
+++ src/component/Stats.jsx
@@ -1,210 +1,227 @@
-import React, {useState, useEffect} from 'react'
-import '../css/stats.css'
-import Selectcountry from './stats/Selectcountry'
-import Today from './stats/Today'
-import Totalcount from './stats/Totalcount'
-import Yesterday from './stats/Yesterday'
-import Chart from './Chart'
+import React, { useState, useEffect } from "react";
+import "../css/stats.css";
+import Selectcountry from "./stats/Selectcountry";
+import Today from "./stats/Today";
+import Totalcount from "./stats/Totalcount";
+import Yesterday from "./stats/Yesterday";
+import Chart from "./Chart";
 
 function Stats() {
+  const [countries, setCountries] = useState([]); //set the countries in the select options
+  const [countryvalue, changecountry] = useState("wrldwide"); // set the value of the select(country) field
+  const [countryInfo, setcountryInfo] = useState({}); //set the country info tp display the total and today report
+  const [yesterdaydata, setyesterdaydata] = useState({}); //set data fo the yesterday
+  const [chartdata, setchartdata] = useState({});
 
-    const [countries, setCountries] = useState([]) //set the countries in the select options
-    const [countryvalue, changecountry] = useState('wrldwide') // set the value of the select(country) field
-    const [countryInfo, setcountryInfo] = useState({}) //set the country info tp display the total and today report
-    const [yesterdaydata, setyesterdaydata] = useState({}) //set data fo the yesterday
-    const [chartdata, setchartdata] = useState({})
+  //set the country info for world on first load
+  useEffect(() => {
+    // for total data and todays data
+    fetch("https://api.caw.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setcountryInfo(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-    //set the country info for world on first load
-    useEffect(() => {
+    // for yesterday data
+    fetch("https://api.caw.sh/v3/covid-19/historical/all?lastdays=2")
+      .then((response) => response.json())
+      .then((data) => {
+        const yesterday = {
+          Cases: Object.values(data.cases)[1] - Object.values(data.cases)[0],
+          Deaths: Object.values(data.deaths)[1] - Object.values(data.deaths)[0],
+          Recovered:
+            Object.values(data.recovered)[1] - Object.values(data.recovered)[0],
+        };
+        setyesterdaydata(yesterday);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-        // for total data and todays data
-        fetch('https://api.caw.sh/v3/covid-19/all')
-        .then((response) => (response.json()))
+    const fetch_chartdata = async () => {
+      await fetch("https://api.caw.sh/v3/covid-19/historical/all?lastdays=30")
+        .then((response) => response.json())
         .then((data) => {
-            setcountryInfo(data);
-        }).catch((error) => {
-            console.log(error)
-        })
-
-        // for yesterday data
-        fetch('https://api.caw.sh/v3/covid-19/historical/all?lastdays=2')
-        .then(response => response.json())
-        .then(data => {
-            const yesterday = {
-                'Cases': Object.values(data.cases)[1] - Object.values(data.cases)[0],
-                'Deaths': Object.values(data.deaths)[1] - Object.values(data.deaths)[0],
-                'Recovered': Object.values(data.recovered)[1] - Object.values(data.recovered)[0]
+          var newchartdata = [];
+          var lastcasedata;
+          var lastdeathdata;
+          var lastrecoverdata;
+          for (var casedata in data.cases) {
+            let newdatapoint;
+            if (lastcasedata && lastdeathdata && lastrecoverdata) {
+              newdatapoint = {
+                date: casedata,
+                cases: data.cases[casedata] - lastcasedata,
+                death: data.deaths[casedata] - lastdeathdata,
+                recovered: data.recovered[casedata] - lastrecoverdata,
+              };
+              newchartdata.push(newdatapoint);
             }
-            setyesterdaydata(yesterday)
-        }).catch((error) => {
-            console.log(error)
+            lastcasedata = data.cases[casedata];
+            lastdeathdata = data.deaths[casedata];
+            lastrecoverdata = data.recovered[casedata];
+            console.log(newdatapoint);
+          }
+          setchartdata(newchartdata);
         })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetch_chartdata();
+  }, []);
 
-        const fetch_chartdata = async () => {
-            await fetch('https://api.caw.sh/v3/covid-19/historical/all?lastdays=30')
-            .then(response => response.json())
-            .then(data => {
-    
-                var newchartdata = []
-                var lastcasedata
-                var lastdeathdata
-                var lastrecoverdata
-                for(var casedata in data.cases){
-                    if(lastcasedata && lastdeathdata && lastrecoverdata){
-                        var newdatapoint = {
-                            date: casedata,
-                            cases: data.cases[casedata] - lastcasedata,
-                            death: data.deaths[casedata] - lastdeathdata,
-                            recovered: data.recovered[casedata] - lastrecoverdata
-                        }
-                        newchartdata.push(newdatapoint)
-                    }
-                    lastcasedata = data.cases[casedata]
-                    lastdeathdata = data.deaths[casedata]
-                    lastrecoverdata = data.recovered[casedata]
-                }
-                setchartdata(newchartdata);
-            }).catch((error) => {
-                console.log(error)
-            })
-        }
-            fetch_chartdata()
-
-    }, [])
-
-    //collect the data to get all the countries in the world
-    useEffect(() => {
-
-        const setcountriesdata = async () => {
-            await fetch('https://api.caw.sh/v3/covid-19/countries')
-            .then((res) => (res.json()))
-            .then((data) => {
-                const countries = data.map((item) => (
-                    {
-                        name: item.country,
-                        value: item.countryInfo.iso3
-                    }
-                    ))
-                    setCountries(countries);
-                }).catch((error) => {
-                    console.log(error)
-                })
-            }
-        setcountriesdata();
-    }, [])
-        
-    //set the change in the select(country) field and the country info
-    const oncountrychange = (event) => {
-
-        const countrycode = event.target.value
-        
-        const url = countrycode === 'wrldwide' ? 'https://api.caw.sh/v3/covid-19/all' : `https://api.caw.sh/v3/covid-19/countries/${countrycode}` //url for country info
-        
-        fetch(url)
-        .then((response) => (response.json()))
+  //collect the data to get all the countries in the world
+  useEffect(() => {
+    const setcountriesdata = async () => {
+      await fetch("https://api.caw.sh/v3/covid-19/countries")
+        .then((res) => res.json())
         .then((data) => {
-            changecountry(countrycode); //changes the select(country) value
-            setcountryInfo(data); //changes the country info
-        }).catch((error) => {
-            console.log(error)
+          const countries = data.map((item) => ({
+            name: item.country,
+            value: item.countryInfo.iso3,
+          }));
+          setCountries(countries);
         })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    setcountriesdata();
+  }, []);
 
-        // for yesterday's data
-        countrycode !== 'wrldwide' ? (
-            fetch(`https://api.caw.sh/v3/covid-19/historical/${countrycode}?lastdays=30`)
-            .then(response => response.json())
-            .then(data => {
-                const casestype = data['timeline']
-                const yesterday = {
-                    'Cases': Object.values(casestype.cases)[29] - Object.values(casestype.cases)[28],
-                    'Deaths': Object.values(casestype.deaths)[29] - Object.values(casestype.deaths)[28],
-                    'Recovered': Object.values(casestype.recovered)[29] - Object.values(casestype.recovered)[28]
-                }
+  //set the change in the select(country) field and the country info
+  const oncountrychange = (event) => {
+    const countrycode = event.target.value;
 
-                var newchartdata = []
-                var lastcasedata
-                var lastdeathdata
-                var lastrecoverdata
-                for(var casedata in data.timeline.cases){
-                    if(lastcasedata && lastdeathdata && lastrecoverdata){
-                        var newdatapoint = {
-                            date: casedata,
-                            cases: data.timeline.cases[casedata] - lastcasedata,
-                            death: data.timeline.deaths[casedata] - lastdeathdata,
-                            recovered: data.timeline.recovered[casedata] - lastrecoverdata
-                        }
-                        newchartdata.push(newdatapoint)
-                    }
-                    lastcasedata = data.timeline.cases[casedata]
-                    lastdeathdata = data.timeline.deaths[casedata]
-                    lastrecoverdata = data.timeline.recovered[casedata]
-                }
-                setchartdata(newchartdata)
+    const url =
+      countrycode === "wrldwide"
+        ? "https://api.caw.sh/v3/covid-19/all"
+        : `https://api.caw.sh/v3/covid-19/countries/${countrycode}`; //url for country info
 
-                setyesterdaydata(yesterday)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        changecountry(countrycode); //changes the select(country) value
+        setcountryInfo(data); //changes the country info
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
-            }).catch((error) => {
-                console.log(error)
-            })
-        ) : (
-            fetch('https://api.caw.sh/v3/covid-19/historical/all?lastdays=30')
-        .then(response => response.json())
-        .then(data => {
-            const yesterday = {
-                'Cases': Object.values(data.cases)[29] - Object.values(data.cases)[28],
-                'Deaths': Object.values(data.deaths)[29] - Object.values(data.deaths)[28],
-                'Recovered': Object.values(data.recovered)[29] - Object.values(data.recovered)[28]
-            }
-
-        var newchartdata = []
-        var lastcasedata
-        var lastdeathdata
-        var lastrecoverdata
-        for(var casedata in data.cases){
-            if(lastcasedata && lastdeathdata && lastrecoverdata){
-                var newdatapoint = {
-                    date: casedata,
-                    cases: data.cases[casedata] - lastcasedata,
-                    death: data.deaths[casedata] - lastdeathdata,
-                    recovered: data.recovered[casedata] - lastrecoverdata
-                }
-                newchartdata.push(newdatapoint)
-            }
-            lastcasedata = data.cases[casedata]
-            lastdeathdata = data.deaths[casedata]
-            lastrecoverdata = data.recovered[casedata]
-        }
-        setchartdata(newchartdata);
-        console.log(newchartdata)
-
-        setyesterdaydata(yesterday)
-        }).catch((error) => {
-            console.log(error)
-        })
+    // for yesterday's data
+    countrycode !== "wrldwide"
+      ? fetch(
+          `https://api.caw.sh/v3/covid-19/historical/${countrycode}?lastdays=30`
         )
-    }
+          .then((response) => response.json())
+          .then((data) => {
+            const casestype = data["timeline"];
+            const yesterday = {
+              Cases:
+                Object.values(casestype.cases)[29] -
+                Object.values(casestype.cases)[28],
+              Deaths:
+                Object.values(casestype.deaths)[29] -
+                Object.values(casestype.deaths)[28],
+              Recovered:
+                Object.values(casestype.recovered)[29] -
+                Object.values(casestype.recovered)[28],
+            };
 
+            var newchartdata = [];
+            var lastcasedata;
+            var lastdeathdata;
+            var lastrecoverdata;
+            for (var casedata in data.timeline.cases) {
+              if (lastcasedata && lastdeathdata && lastrecoverdata) {
+                var newdatapoint = {
+                  date: casedata,
+                  cases: data.timeline.cases[casedata] - lastcasedata,
+                  death: data.timeline.deaths[casedata] - lastdeathdata,
+                  recovered:
+                    data.timeline.recovered[casedata] - lastrecoverdata,
+                };
+                newchartdata.push(newdatapoint);
+              }
+              lastcasedata = data.timeline.cases[casedata];
+              lastdeathdata = data.timeline.deaths[casedata];
+              lastrecoverdata = data.timeline.recovered[casedata];
+            }
+            setchartdata(newchartdata);
 
-    return (
-        <div id='stats' className='section'>
-            <Selectcountry oncountrychange={oncountrychange} countryvalue={countryvalue} countries={countries} />
+            setyesterdaydata(yesterday);
+          })
+          .catch((error) => {
+            console.log(error);
+          })
+      : fetch("https://api.caw.sh/v3/covid-19/historical/all?lastdays=30")
+          .then((response) => response.json())
+          .then((data) => {
+            const yesterday = {
+              Cases:
+                Object.values(data.cases)[29] - Object.values(data.cases)[28],
+              Deaths:
+                Object.values(data.deaths)[29] - Object.values(data.deaths)[28],
+              Recovered:
+                Object.values(data.recovered)[29] -
+                Object.values(data.recovered)[28],
+            };
 
-            <Totalcount countryInfo={countryInfo} />
+            var newchartdata = [];
+            var lastcasedata;
+            var lastdeathdata;
+            var lastrecoverdata;
+            for (var casedata in data.cases) {
+              if (lastcasedata && lastdeathdata && lastrecoverdata) {
+                var newdatapoint = {
+                  date: casedata,
+                  cases: data.cases[casedata] - lastcasedata,
+                  death: data.deaths[casedata] - lastdeathdata,
+                  recovered: data.recovered[casedata] - lastrecoverdata,
+                };
+                newchartdata.push(newdatapoint);
+              }
+              lastcasedata = data.cases[casedata];
+              lastdeathdata = data.deaths[casedata];
+              lastrecoverdata = data.recovered[casedata];
+            }
+            setchartdata(newchartdata);
+            console.log(newchartdata);
 
-            <h2 style={{marginBottom: '20px'}}>Today</h2>
-            <Today countryInfo={countryInfo} />
+            setyesterdaydata(yesterday);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+  };
 
-            <h2 style={{marginBottom: '20px'}}>Yesterday</h2>
-            <Yesterday yesterdaydata={yesterdaydata} />
+  return (
+    <div id="stats" className="section">
+      <Selectcountry
+        oncountrychange={oncountrychange}
+        countryvalue={countryvalue}
+        countries={countries}
+      />
 
-            <div className="chart-container">
-                <div className="chart">
-                    <Chart data={chartdata} />
-                </div>
-            </div>
+      <Totalcount countryInfo={countryInfo} />
+
+      <h2 style={{ marginBottom: "20px" }}>Today</h2>
+      <Today countryInfo={countryInfo} />
+
+      <h2 style={{ marginBottom: "20px" }}>Yesterday</h2>
+      <Yesterday yesterdaydata={yesterdaydata} />
+
+      <div className="chart-container">
+        <div className="chart">
+          <Chart data={chartdata} />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-
-export default Stats
+export default Stats;
--- src/component/WorldTable.js
+++ src/component/WorldTable.js
@@ -1,75 +1,74 @@
-import React, {useState, useEffect} from 'react'
-import '../css/worldtable.css'
-import Chart from './Chart'
+import React, { useState, useEffect } from "react";
+import "../css/worldtable.css";
+import Chart from "./Chart";
 
 function WorldTable() {
-    
-    const [tableData, setTableData] = useState([{}])
-    const [chartdata, setchartdata] = useState({})
+  const [tableData, setTableData] = useState([{}]);
+  const [chartdata, setchartdata] = useState({});
 
-    useEffect(() => {
-        const setcountriesdata = async () => {
-            await fetch('https://api.caw.sh/v3/covid-19/countries')
-            .then((res) => (res.json()))
-            .then((data) => {
-                const sorteddata = data.sort((a, b) => (b.cases - a.cases))
-                setTableData(sorteddata);
-            }).catch((error) => {
-                console.log(error)
-            })
-        }
-        setcountriesdata();
-
-        const fetch_chartdata = async () => {
-        await fetch('https://api.caw.sh/v3/covid-19/historical/all?lastdays=30')
-        .then(response => response.json())
-        .then(data => {
-
-            var newchartdata = []
-            var lastcasedata
-            var lastdeathdata
-            var lastrecoverdata
-            for(var casedata in data.cases){
-                if(lastcasedata && lastdeathdata && lastrecoverdata){
-                    var newdatapoint = {
-                        date: casedata,
-                        cases: data.cases[casedata] - lastcasedata,
-                        death: data.deaths[casedata] - lastdeathdata,
-                        recovered: data.recovered[casedata] - lastrecoverdata
-                    }
-                    newchartdata.push(newdatapoint)
-                }
-                lastcasedata = data.cases[casedata]
-                lastdeathdata = data.deaths[casedata]
-                lastrecoverdata = data.recovered[casedata]
-            }
-            setchartdata(newchartdata);
-        }).catch((error) => {
-            console.log(error)
+  useEffect(() => {
+    const setcountriesdata = async () => {
+      await fetch("https://api.caw.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const sorteddata = data.sort((a, b) => b.cases - a.cases);
+          setTableData(sorteddata);
         })
-    }
-        fetch_chartdata()
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    setcountriesdata();
 
-    }, [])
+    const fetch_chartdata = async () => {
+      await fetch("https://api.caw.sh/v3/covid-19/historical/all?lastdays=30")
+        .then((response) => response.json())
+        .then((data) => {
+          var newchartdata = [];
+          var lastcasedata;
+          var lastdeathdata;
+          var lastrecoverdata;
+          for (var casedata in data.cases) {
+            if (lastcasedata && lastdeathdata && lastrecoverdata) {
+              let newdatapoint = {
+                date: casedata,
+                cases: data.cases[casedata] - lastcasedata,
+                death: data.deaths[casedata] - lastdeathdata,
+                recovered: data.recovered[casedata] - lastrecoverdata,
+              };
+              newchartdata.push(newdatapoint);
+              console.log(newdatapoint);
+            }
+            lastcasedata = data.cases[casedata];
+            lastdeathdata = data.deaths[casedata];
+            lastrecoverdata = data.recovered[casedata];
+          }
+          console.log(newchartdata);
+          setchartdata(newchartdata);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetch_chartdata();
+  }, []);
 
-    return (
-        <div id='worldtable' class='worldtable'>
-            <h2>Worldwide</h2>
-            <div className="table">
-                {
-                    tableData.map((item) => (
-                        <div className="row">
-                            <div className="country-name">{item.country}</div>
-                            <div className="country-cases">{item.cases}</div>
-                        </div>
-                    ))
-                }
-            </div>
-            <div className="graph">
-                <Chart data={chartdata} />
-            </div>
-        </div>
-    )
+  return (
+    <div id="worldtable" class="worldtable">
+      <h2>Worldwide</h2>
+      <div className="table">
+        {tableData.map((item) => (
+          <div className="row">
+            <div className="country-name">{item.country}</div>
+            <div className="country-cases">{item.cases}</div>
+          </div>
+        ))}
+      </div>
+      <div className="graph">
+        <Chart data={chartdata} />
+      </div>
+    </div>
+  );
 }
 
-export default WorldTable
+export default WorldTable;
